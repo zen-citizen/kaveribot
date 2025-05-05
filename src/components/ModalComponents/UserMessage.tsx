2f182c840670d10202f186de685418a32221f861
vijaycru <89444413+vijaycru@users.noreply.github.com>
2025-05-06 03:41:46 +0530

    Delete src directory

--- src/components/ModalComponents/UserMessage.tsx
+++
@@ -1,7 +0,0 @@
-export const UserMessage = ({ value }: { value: string }) => {
-  return (
-    <div className="message-text text-neutral-50 text-sm bg-[#013df5] p-3 rounded-lg max-w-10/12 ml-auto tracking-wide leading-[22px]">
-      {value}
-    </div>
-  );
-};
