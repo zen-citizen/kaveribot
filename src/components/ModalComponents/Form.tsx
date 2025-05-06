0291c2c56506c7a6f094474a39764558f95b2d43
pritipg <pritipg@users.noreply.github.com>
2025-05-06 20:22:13 +0100

    Form revisions

--- src/components/ModalComponents/Form.tsx
+++ src/components/ModalComponents/Form.tsx
@@ -18,14 +18,14 @@ export const Form = ({
 }: ChatInputProps) => {
   const textValue = inputRef.current?.value.trim();
   return (
-    <div className="tw:w-full tw:flex tw:flex-row tw:items-center tw:bg-white tw:h-13">
+    <div className="tw:w-full tw:flex tw:flex-row tw:items-center tw:bg-white tw:h-13 tw:px-2">
       <textarea
         ref={inputRef}
         autoFocus
         id="chat-input"
         value={message}
         onChange={(e) => setMessage(e?.target?.value)}
-        className="tw:w-full tw:p-2 tw:ml-2 tw:text-sm tw:h-3/4 tw:rounded-md tw:focus:outline-none tw:focus:ring-2 tw:focus:ring-blue-500 tw:resize-none"
+        className="tw:w-full tw:p-2 tw:ml-2 tw:text-sm tw:h-3/4 tw:rounded-md tw:focus:outline-none tw:focus:ring-2 tw:focus:ring-[#003df5]/60 tw:resize-none"
         placeholder="Ask a question"
         onKeyDown={(event) => {
           if (formEvent.loading) return;
@@ -38,7 +38,14 @@ export const Form = ({
         disabled={formEvent.loading}
       ></textarea>
       <div className="chat-controls tw:flex tw:items-center tw:justify-between tw:p-2 tw:cursor-pointer">
-        <Mic className={textValue ? "tw:hidden" : "tw:block"} id="mic-button" />
+        <Mic
+          className={
+            textValue
+              ? "tw:hidden"
+              : "tw:block tw:text-gray-500 tw:hover:text-gray-700 tw:transition"
+          }
+          id="mic-button"
+        />
         <SendHorizontal
           onClick={(event) => {
             if (formEvent.loading) return;
@@ -46,7 +53,11 @@ export const Form = ({
             setMessage("");
             event.preventDefault();
           }}
-          className={textValue ? "tw:block" : "tw:hidden"}
+          className={
+            textValue
+              ? "tw:block tw:text-gray-500 tw:hover:text-gray-700 tw:transition"
+              : "tw:hidden"
+          }
         />
       </div>
     </div>
