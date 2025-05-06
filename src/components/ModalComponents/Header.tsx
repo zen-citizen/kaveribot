b925ce911c854e53e3597457d24aa4994f07e8ca
pritipg <pritipg@users.noreply.github.com>
2025-05-06 20:02:18 +0100

    Header revisions

--- src/components/ModalComponents/Header.tsx
+++ src/components/ModalComponents/Header.tsx
@@ -5,18 +5,17 @@ interface BodyProps {
   togglePopup: boolean;
 }
 
-export const Header = ({
-  setTogglePopup,
-  togglePopup,
-}: BodyProps) => {
+export const Header = ({ setTogglePopup, togglePopup }: BodyProps) => {
   return (
     <header className="chat-header tw:flex tw:justify-between tw:items-center tw:border-b tw:border-gray-200 tw:px-4 tw:py-3 tw:bg-black">
       <div className="header-info tw:flex tw:items-center tw:gap-3">
-        <CircleEllipsis className="tw:text-white" size={40} strokeWidth={1.5} />
+        <CircleEllipsis className="tw:text-white" size={30} strokeWidth={1.5} />
         <h2 className="logo-text tw:text-md tw:font-semibold tw:text-white tw:text-underline">
           Ask Zen Citizen
         </h2>
-        <span className="tw:bg-gray-200 tw:border tw:px-2 tw:rounded-md tw:text-[#013df5]">Beta</span>
+        <span className="tw:bg-gray-200 tw:px-2 tw:py-0.5 tw:rounded-sm tw:text-xs tw:text-[#003df5]">
+          Beta
+        </span>
       </div>
       <ChevronDown
         className="tw:text-white tw:cursor-pointer"
--- src/components/ModalComponents/UserMessage.tsx
+++ src/components/ModalComponents/UserMessage.tsx
@@ -1,6 +1,6 @@
 export const UserMessage = ({ value }: { value: string }) => {
   return (
-    <div className="message-text tw:text-neutral-50 tw:text-sm tw:bg-[#013df5] tw:px-5 tw:py-4 tw:rounded-lg tw:max-w-10/12 tw:ml-auto">
+    <div className="message-text tw:text-neutral-50 tw:text-sm tw:bg-[#003df5] tw:px-5 tw:py-4 tw:rounded-lg tw:max-w-10/12 tw:ml-auto">
       {value}
     </div>
   );
