1cff94fec445548d9c919fe76e0edd6d443875b4
Chandru <chandru89new@gmail.com>
2025-05-07 09:57:48 +0530

    Fix styles.

--- src/components/ModalComponents/BotMessage.tsx
+++ src/components/ModalComponents/BotMessage.tsx
@@ -3,9 +3,9 @@ import { FeedbackContainer } from "./index";
 export const BotMessage = ({ value }: { value: string }) => {
   return (
     <div className="tw:flex-col tw:flex tw:gap-2">
-      <p className="tw:text-gray-500 tw:text-xs tw:font-medium">
+      <div className="tw:text-gray-500 tw:text-xs tw:font-medium">
         Zen Citizen Bot
-      </p>
+      </div>
       <div className="message-text response-text tw:text-sm tw:text-gray-800 tw:bg-white tw:px-5 tw:rounded-lg tw:text-left tw:py-4">
         {value}
       </div>
