ce89ebafd090d633d8b67f99b3725ff62bb6e668
Chandru <chandru89new@gmail.com>
2025-05-09 11:55:53 +0530

    Update logo.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   build: {
     rollupOptions: {
       output: {
