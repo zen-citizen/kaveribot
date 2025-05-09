10a9caea5ac2e544cf037c2785ec1201573b84a3
Chandru <chandru89new@gmail.com>
2025-05-09 11:55:53 +0530

    Update logo.

--- src/components/ModalComponents/Header.tsx
+++ src/components/ModalComponents/Header.tsx
@@ -1,4 +1,5 @@
-import { ChevronDown, CircleEllipsis } from "lucide-react";
+import { ChevronDown } from "lucide-react";
+import ZcLogo from "../../assets/zc-logo.svg?react";
 
 interface HeaderProps {
   setTogglePopup: React.Dispatch<React.SetStateAction<boolean>>;
@@ -9,13 +10,13 @@ export const Header = ({ setTogglePopup, togglePopup }: HeaderProps) => {
   return (
     <header className="chat-header tw:flex! tw:justify-between! tw:items-center! tw:border-b tw:border-gray-200 tw:px-4 tw:py-3 tw:bg-black">
       <div className="header-info tw:flex! tw:items-center! tw:gap-2 tw:sm:gap-3 tw:flex-1! tw:overflow-hidden">
-        <CircleEllipsis
+        <ZcLogo
           className="tw:text-white !min-w-[30px]"
-          size={30}
+          width={30}
           strokeWidth={1.5}
         />
         <h2 className="logo-text tw:text-md tw:font-semibold tw:text-white tw:text-underline tw:mb-0! tw:whitespace-nowrap">
-          Ask Zen Citizen
+          Ask Zen
         </h2>
         <span className="tw:bg-gray-200 tw:px-2 tw:py-0.5 tw:rounded-sm tw:text-xs tw:text-[#003df5]">
           Beta
