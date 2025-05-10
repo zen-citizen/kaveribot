81589c3196a11d2b438e004dd010282e0359abd9
Chandru <chandru89new@gmail.com>
2025-05-10 18:57:21 +0530

    Release v1.1.0 with name changes.

--- src/components/ModalComponents/BotMessage.tsx
+++ src/components/ModalComponents/BotMessage.tsx
@@ -134,7 +134,7 @@ export const BotMessage = ({ value }: { value: string }) => {
   return (
     <div className="tw:flex-col tw:flex tw:gap-2">
       <div className="tw:text-gray-500 tw:text-xs tw:font-medium">
-        Zen Citizen Bot
+        Zen Citizen
       </div>
       <div className="message-text response-text tw:text-sm tw:text-gray-800 tw:bg-white tw:px-5 tw:rounded-lg tw:text-left tw:py-4">
         <div className="markdown">
