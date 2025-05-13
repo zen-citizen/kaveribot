924589778e27643f46b28ffdeaf7bd19510ced26
arpangupta1805 <guptaarpan1805@gmail.com>
2025-05-13 11:16:28 +0530

    Added the sidepannel feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ChatButton from "./components/ChatButton";
 import ChatPopup from "./components/ChatPopup";
 import { AppContext, AppState } from "./AppState";
 
 
 function App() {
-  const [togglePopup, setTogglePopup] = useState(true);
+  const [togglePopup, setTogglePopup] = useState(false);
   const [messages, setMessages] = useState<Array<{ role: string; message: string }>>([]);
   const featureFlags: AppContext["featureFlags"] = { langSupport: false }
+  
+  useEffect(() => {
+    // Add or remove class from html element to shift content
+    const htmlElement = document.documentElement;
+    if (togglePopup) {
+      htmlElement.classList.add('kaveri-bot-active');
+    } else {
+      htmlElement.classList.remove('kaveri-bot-active');
+    }
+  }, [togglePopup]);
+  
   return (
     <AppState.Provider value={{ messages, setMessages, featureFlags }}>
       <div id="kaveri-bot-app" className="app-container relative! z-[10000000]">
         <ChatButton togglePopup={togglePopup} setTogglePopup={setTogglePopup} />
-        {togglePopup && <ChatPopup setTogglePopup={setTogglePopup} togglePopup={togglePopup}/>}
+        <ChatPopup setTogglePopup={setTogglePopup} togglePopup={togglePopup}/>
       </div>
     </AppState.Provider>
   );
--- src/components/ChatButton.tsx
+++ src/components/ChatButton.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown, MessageSquareText } from "lucide-react";
+import { ChevronRight, MessageSquareText } from "lucide-react";
 
 interface ChatButtonProps {
   setTogglePopup: React.Dispatch<React.SetStateAction<boolean>>;
@@ -6,12 +6,13 @@ interface ChatButtonProps {
 }
 
 const ChatButton = ({ togglePopup, setTogglePopup }: ChatButtonProps) => {
-  const IconToggle = togglePopup ? <ChevronDown /> : <MessageSquareText />;
+  const IconToggle = togglePopup ? <ChevronRight /> : <MessageSquareText />;
+  
   return (
     <button
       id="chatbot-toggler"
       onClick={() => setTogglePopup(!togglePopup)}
-      className="tw:fixed tw:bottom-4 tw:right-4 tw:bg-black! tw:text-white! tw:p-4 tw:rounded-full! tw:shadow-lg! tw:focus:outline-none! tw:cursor-pointer"
+      className="tw:fixed tw:bottom-4 tw:right-4 tw:bg-black! tw:text-white! tw:p-4 tw:rounded-full! tw:shadow-lg! tw:focus:outline-none! tw:cursor-pointer tw:z-[10000001]"
     >
       {IconToggle}
     </button>
