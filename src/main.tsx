2a4c35b801953d6cb899906f4acef43c86a700ad
Chandru <chandru89new@gmail.com>
2025-05-17 19:07:20 +0530

    Move posthog into assets so it's bundled.

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,16 @@ under certain conditions.
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import posthogScript from "./assets/posthog.js?url"
 
 import App from "./App.tsx";
 
 const container = document.createElement("div");
 container.id = "react-root";
 document.body.appendChild(container);
+const script = document.createElement("script");
+script.src = posthogScript;
+document.head.appendChild(script);
 
 createRoot(container).render(
   <StrictMode>
