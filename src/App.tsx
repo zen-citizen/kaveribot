8493ad3b659e1840261f5e2775e3b27cf90f6bb7
Chandru <chandru89new@gmail.com>
2025-05-31 15:24:30 +0530

    Add licence.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
+/**
+
+"Ask Zen Citizen" Copyright (C) 2025 Zen Citizen
+
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+ */
 import { useState } from "react";
 import ChatButton from "./components/ChatButton";
 import ChatPopup from "./components/ChatPopup";
 import { AppContext, AppState } from "./AppState";
 
-
 function App() {
   const [togglePopup, setTogglePopup] = useState(true);
-  const [messages, setMessages] = useState<Array<{ role: string; message: string }>>([]);
-  const featureFlags: AppContext["featureFlags"] = { langSupport: false }
+  const [messages, setMessages] = useState<
+    Array<{ role: string; message: string }>
+  >([]);
+  const featureFlags: AppContext["featureFlags"] = { langSupport: false };
   return (
     <AppState.Provider value={{ messages, setMessages, featureFlags }}>
       <div id="kaveri-bot-app" className="app-container relative! z-[10000000]">
         <ChatButton togglePopup={togglePopup} setTogglePopup={setTogglePopup} />
-        {togglePopup && <ChatPopup setTogglePopup={setTogglePopup} togglePopup={togglePopup}/>}
+        {togglePopup && (
+          <ChatPopup
+            setTogglePopup={setTogglePopup}
+            togglePopup={togglePopup}
+          />
+        )}
       </div>
     </AppState.Provider>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,11 @@
+/**
+
+"Ask Zen Citizen" Copyright (C) 2025 Zen Citizen
+
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+ */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -8,6 +16,8 @@ const container = document.createElement("div");
 container.id = "react-root";
 document.body.appendChild(container);
 
-createRoot(container).render(<StrictMode>
+createRoot(container).render(
+  <StrictMode>
     <App />
-  </StrictMode>)
+  </StrictMode>
+);
